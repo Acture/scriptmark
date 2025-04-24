4d0c84da9ba8074dba4b8e1068ca94c176c9cd0e
acture <acturea@gmail.com>
2025-04-25 04:03:09 +0800

    feat(submission): add hash caching and update functionality
    Introduce `cached_hash` field in `Submission` struct and a new method `update_hash` to compute and store the hash of the file at `submission_path`. Additionally, change the type of `submission_path` from `String` to `PathBuf` for better filesystem path handling.
    
    Signed-off-by: acture <acturea@gmail.com>

--- crates/grader/src/defines/assignment.rs
+++ crates/grader/src/defines/assignment.rs
@@ -1,6 +1,5 @@
 use crate::defines::task::Task;
 use serde::{Deserialize, Serialize};
-use std::fmt::{Display, Formatter};
 use std::hash::Hash;
 use typed_builder::TypedBuilder;
 
--- crates/grader/src/defines/submission.rs
+++ crates/grader/src/defines/submission.rs
@@ -1,8 +1,26 @@
 use serde::{Deserialize, Serialize};
+use std::hash::{DefaultHasher, Hash, Hasher};
+use std::path::PathBuf;
 use typed_builder::TypedBuilder;
 
 #[derive(Debug, TypedBuilder, Serialize, Deserialize, Clone)]
 pub struct Submission {
 	pub score: Option<f64>,
-	pub submission_path: String,
+	pub submission_path: PathBuf,
+	pub cached_hash: Option<u64>,
+}
+
+impl Submission {
+	pub fn update_hash(&mut self) {
+		if !self.submission_path.exists() {
+			panic!("Submission path does not exist: {}", self.submission_path.display());
+		}
+
+
+		let contents = std::fs::read(self.submission_path.as_path()).unwrap();
+		let mut hasher = DefaultHasher::new();
+		contents.hash(&mut hasher);
+		let hash = hasher.finish();
+		self.cached_hash = Some(hash);
+	}
 }
